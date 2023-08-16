5a3c45c3cd8a995e8c561a3f4b818135a8dc4508
cxr <claire_129as@yahoo.com.tw>
2023-08-16 08:06:53 +0800

    rewrite vue.config.js

--- vue.config.js
+++ vue.config.js
@@ -1,5 +1,5 @@
-module.exports = {
-  publicPath: process.env.NODE_ENV === 'production'
-    ? '/brunch-cloud/'
-    : '/'
-}
+const { defineConfig } = require('@vue/cli-service')
+module.exports = defineConfig({
+  transpileDependencies: true,
+  publicPath: '/brunch-cloud/'
+})
